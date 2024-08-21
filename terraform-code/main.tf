4221cef170651d7ce1489721a36828999338cfab
Saif178 <mehdi.saiful@gmail.com>
2024-08-21 17:17:21 +0530

    add resources

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -4,3 +4,11 @@ resource "github_repository" "mtc-repo" {
   visibility  = "private"
   auto_init   = true
 }
+
+resource "github_repository_file" "readme" {
+  repository          = github_repository.mtc-repo.name
+  branch              = "main"
+  file                = "README.md"
+  content             = "# This repo is for infra devlopers."
+  overwrite_on_create = true
+}
