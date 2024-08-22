df1a1121e89589b8cb71b85544230e2d4bcfdd1f
Saif178 <mehdi.saiful@gmail.com>
2024-08-22 10:51:22 +0530

    added random resource and index.html

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -6,9 +6,21 @@ resource "github_repository" "mtc-repo" {
 }
 
 resource "github_repository_file" "readme" {
-    repository = github_repository.mtc-repo.name
-    branch = "main"
-    file = "README.md"
-    content = "# This repo is for infra devlopers."
-    overwrite_on_create = true
+  repository          = github_repository.mtc-repo.name
+  branch              = "main"
+  file                = "README.md"
+  content             = "# This repo is for infra devlopers."
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "index" {
+  repository          = github_repository.mtc-repo.name
+  branch              = "main"
+  file                = "index.html"
+  content             = "<doc><body><h1>hello Terraform</h1></body></doc>"
+  overwrite_on_create = true
+}
+
+resource "random_id" "random" {
+  byte_length = 2
 }
