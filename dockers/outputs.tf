867e43fe04b1334bfcc53f913565fc72dfb65f5c
Saif178 <mehdi.saiful@gmail.com>
2024-08-26 20:26:16 +0530

    added new module container and a bash script for local-exec in root module

--- dockers/outputs.tf
+++ dockers/outputs.tf
@@ -1,14 +1,14 @@
-output "container_name" {
-  value       = docker_container.nodered_container[*].name
-  description = "the name of the containers"
-}
+#output "container_name" {
+#  value       = docker_container.nodered_container[*].name
+#  description = "the name of the containers"
+#}
 
-output "ip_address" {
-  value = flatten([
-    for i in docker_container.nodered_container[*] : [
-      for ip, port in zipmap(i.network_data[*].ip_address, i.ports[*].external) : join(":", [ip, tostring(port)])
-    ]
-  ])
-  description = "the ip address and external port of the containers"
-  sensitive   = false
-}
+#output "ip_address" {
+#  value = flatten([
+#    for i in docker_container.nodered_container[*] : [
+#      for ip, port in zipmap(i.network_data[*].ip_address, i.ports[*].external) : join(":", [ip, tostring(port)])
+#    ]
+#  ])
+#  description = "the ip address and external port of the containers"
+#  sensitive   = false
+#}
---
+++ dockers/script.bat
@@ -0,0 +1,4 @@
+@echo off
+timeout /t 60
+mkdir noderedvol
+icacls noderedvol /grant Saif:F /T
