e9cdd63e3093365958d77529babbbc535c9650f4
Saif178 <mehdi.saiful@gmail.com>
2024-09-02 18:44:02 +0530

    modified different files

--- terraform-ci-cd/mtc-control/deployments/mtc-dev/main.tf
+++ terraform-ci-cd/mtc-control/deployments/mtc-dev/main.tf
@@ -7,7 +7,7 @@
 // Modules
 module "compute" {
   source  = "app.terraform.io/TDC-178/compute/aws"
-  version = "1.0.0"
+  version = "1.0.1"
 
   aws_region = "us-east-1"
   public_key_material = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMW5uJwSdHX8Bv9gbCNryoaDLLSTUJQnGuLr2dBe5hkU/91qtyBTh50+G/L1G5asst6eqnLUR8LdDGVoHBmUOb35jjrhju0OYB0Rtyk3NbYSkAEvGjU5LNgtVqGl75PdcTIgGvo9H99ykiwAgVOZFLOWyH/1FaXBWQzS6Urjw8KP1fZuJfnk3jHfgMpoYNZddfJoWAl7yJ+fmjLOgr0D6uM4aeA1h9HknhOoyjsbw18FqwxVSri8dsXSViAVdC38EG7NWH/tC7pUp8fLg5VqOqq0zGLOusowWsRsBPlYHAlN+ywwFG7IRL5dqy5QfSSp/cTpoq5aiwGgma+gDixm4TZ/2H0baPA+KfGOBEdXpn8CncczRqHEtz+TPabFzucFCsFZzRfBBiVzUqdZFbP+LNBzFzK8cIkoj/5FOFfrFKASIDfax9qnA4En6JZwfwGhZYroMuNy3aXPUYrIP//Pv/5PXaJ72P5+bGzWho6N7sfSGVObOyiVmYMUdixVYtmIU= saif@Saif178"
